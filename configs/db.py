0584feb99acd8ffb511960a723e175c1470b018d
ridoineel <ridoineelofficiel@gmail.com>
2022-04-13 18:52:05 +0000

    update db configurations

--- configs/db.py
+++ configs/db.py
@@ -1,10 +1,13 @@
 from pymongo import MongoClient
+from dotenv import dotenv_values
 
-# put your mongodb uri here
-# con = MongoClient(DB_URI)
+ # get database uri from .env file
+DB_URI = dotenv_values().get("DB_URI")
 
-# if u wanna to use local
-con = MongoClient()
+if DB_URI:
+	con = MongoClient(DB_URI)
+else:
+	con = MongoClient()
 
-# by default
+# by default bb name is local
 db = con.local
